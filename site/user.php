11c375a787e8fb77ba48a8e6c79c805fc819d0b7
Bruno dos Santos Oliveira <brunoso2006@gmail.com>
2024-09-12 21:53:26 -0300

    Atualização Final do projeto

--- site/user.php
+++ site/user.php
@@ -11,19 +11,63 @@
         <div class="content">
             <header>
                 <div class="leftSide">
-                    LOGO
+                    <img src="imgs/logo.png" alt="Logo Galaxy Stars" id="logo">
                 </div>
 
                 <nav class="mid">
-                        <a href="#">Home</a>
-                        <a href="#">Criador</a>
-                        <a href="#">Desconectar</a>
+                        <a href="user.php">Home</a>
+                        <a href="https://github.com/BrunoSantosOliveira">Criador</a>
                 </nav>
 
                 <div class="rightSide">
                     <button><a href="index.php">Desconectar</a></button>
                 </div>
             </header>
+
+            <div class="mainContent">
+            <div class="row">
+                <div class="site">
+                    <div class="topo">
+                        <h3>Galáxia 3D</h3>
+                        <button><a href="https://stars.chromeexperiments.com/">Visitar</a></button>
+                    </div>
+                    <pre>Ótimo site para visualizar as 100 Mil estrelas
+mais próximas do Sol, Site do Chrome Experiments.</pre>
+                </div>
+
+                <div class="site">
+                    <div class="topo">
+                        <h3>Nasa</h3>
+                        <button><a href="https://www.nasa.gov/">Visitar</a></button>
+                    </div>
+                    <pre>Um site da ORG da Nasa para que você se atualize
+de muitas coisas que acontecem no universo.</pre>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="site">
+                    <div class="topo">
+                        <h3>Hubblesite</h3>
+                        <button><a href="https://hubblesite.org/">Visitar</a></button>
+                    </div>
+                    <pre>O site oficial do Telescópio Espacial Hubble
+contém uma enorme galeria de imagens
+impressionantes de estrelas e galáxias.</pre>
+                </div>
+
+                <div class="site">
+                    <div class="topo">
+                        <h3>Space.com</h3>
+                        <button><a href="https://www.space.com/">Visitar</a></button>
+                    </div>
+                    <pre>O Space.com é um site popular de notícias de
+astronomia que cobre uma variedade de tópicos
+relacionados a estrelas, galáxias e descobertas
+recentes no espaço.</pre>
+                </div>
+            </div>
+            </div>
         </div>
 </body>
 </html>
